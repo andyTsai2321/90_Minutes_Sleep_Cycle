9d3a00e6d7ad07eb4e3d0e123a76c3af277922af
andyTsai2321 <andytsai@caihede-MacBook-Pro.local>
2020-05-23 03:11:37 +0800

    Updates

---
+++ precache-manifest.7fce0aaac6a418768a19c0372a5d7e94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc836de9aba248951fa5c88d3b99535",
+    "url": "/90_Minutes_Sleep_Cycle/index.html"
+  },
+  {
+    "revision": "d1dc076f50ef61773cef",
+    "url": "/90_Minutes_Sleep_Cycle/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "0dc37454bf2fdc34c1f4",
+    "url": "/90_Minutes_Sleep_Cycle/static/css/main.3563c667.chunk.css"
+  },
+  {
+    "revision": "d1dc076f50ef61773cef",
+    "url": "/90_Minutes_Sleep_Cycle/static/js/2.84811785.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/90_Minutes_Sleep_Cycle/static/js/2.84811785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc37454bf2fdc34c1f4",
+    "url": "/90_Minutes_Sleep_Cycle/static/js/main.5d751795.chunk.js"
+  },
+  {
+    "revision": "db9482a61322d369cbe0",
+    "url": "/90_Minutes_Sleep_Cycle/static/js/runtime-main.4d1a1885.js"
+  }
+]);
